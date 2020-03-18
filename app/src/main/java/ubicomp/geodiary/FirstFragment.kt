2ec07cb471e8664ed3197d21efc7dc895d77f7cb
TSavusal <44393323+TSavusal@users.noreply.github.com>
2020-03-18 14:57:15 +0200

    Background Input section Main menu

--- app/src/main/java/ubicomp/geodiary/FirstFragment.kt
+++ app/src/main/java/ubicomp/geodiary/FirstFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import kotlin.system.exitProcess
+import android.widget.Button
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
