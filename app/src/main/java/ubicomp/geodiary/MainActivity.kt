9f898a5fdcda581bb08218c0456c8def9a72fc21
TSavusal <44393323+TSavusal@users.noreply.github.com>
2020-03-24 15:55:59 +0200

    RecyclerView added to xml

--- app/src/main/java/ubicomp/geodiary/MainActivity.kt
+++ app/src/main/java/ubicomp/geodiary/MainActivity.kt
@@ -14,17 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
