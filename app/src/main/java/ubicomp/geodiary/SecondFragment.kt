0a09613490fce0e2b6376369ef4c5a5f3f9302e4
TSavusal <44393323+TSavusal@users.noreply.github.com>
2020-03-27 12:37:21 +0200

    Added more google maps activity

--- app/src/main/java/ubicomp/geodiary/SecondFragment.kt
+++ app/src/main/java/ubicomp/geodiary/SecondFragment.kt
@@ -1,14 +1,18 @@
 package ubicomp.geodiary
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_second.*
+import java.util.*
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -30,6 +34,16 @@ class SecondFragment : Fragment() {
 
         view.findViewById<Button>(R.id.button_input).setOnClickListener {
             val EntryText = EditEntry.text
+
+            val gcd = Geocoder(context, Locale.getDefault())
+            var lng = 0.0
+            var lat = 0.0
+            val addresses: List<Address> = gcd.getFromLocation(lat, lng, 1)
+            if (addresses.size > 0) {
+                System.out.println(addresses[0].getLocality())
+            } else {
+                // do your stuff
+            }
         }
 
         view.findViewById<Button>(R.id.button_back).setOnClickListener {
