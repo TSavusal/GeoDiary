5ca8981320a05cdb9be876f49144361763ef55e6
msavusal <markus.savusalo@windowslive.com>
2020-04-15 18:59:30 +0300

    database converter

---
+++ app/src/main/java/ubicomp/geodiary/Converters.java
@@ -0,0 +1,21 @@
+package ubicomp.geodiary;
+
+import androidx.room.TypeConverter;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class Converters {
+    @TypeConverter
+    public static ArrayList<String> fromString(String value) {
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static String fromArrayList(ArrayList<String> list) {
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return json;
+    }
+}
