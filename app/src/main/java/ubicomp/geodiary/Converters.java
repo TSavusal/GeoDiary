ef7a97cc35b3513d3a3c0263ea9ad9b320e53852
msavusal <markus.savusalo@windowslive.com>
2020-04-20 19:38:57 +0300

    Merged Database feature branch to master

--- app/src/main/java/ubicomp/geodiary/Converters.java
+++ app/src/main/java/ubicomp/geodiary/Converters.java
@@ -2,20 +2,21 @@ package ubicomp.geodiary;
 
 import androidx.room.TypeConverter;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class Converters {
     @TypeConverter
-    public static ArrayList<String> fromString(String value) {
+    public static void fromString(String value) {
+        /*
         Type listType = new TypeToken<ArrayList<String>>() {}.getType();
         return new Gson().fromJson(value, listType);
+        */
     }
 
     @TypeConverter
-    public static String fromArrayList(ArrayList<String> list) {
-        Gson gson = new Gson();
-        String json = gson.toJson(list);
-        return json;
+    public static void fromArrayList(ArrayList<String> list) {
+        //Gson gson = new Gson();
+        //String json = gson.toJson(list);
+        //return json;
     }
 }
